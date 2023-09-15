a78efa8859a07c4c1bd6ee9765a1b1cc54cd172e
losc06001 <losc06001@E-PW0234JE>
2023-09-15 13:01:15 +0200

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgift2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket tjänar den första personen?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hur mycket tjänar den andra personen?");
+            string y = Console.ReadLine();
+            Console.WriteLine("Hur mycket tjänar den tredje personen?");
+            string z = Console.ReadLine();
+            double första = double.Parse(x);
+            double andra = double.Parse(y);
+            double tredje = double.Parse(z);
+            double sum =  (första + andra + tredje);
+            Console.WriteLine("Medellönen på personalen är " + sum/2 + "kr");
+            Console.ReadKey();
+        }
+    }
+}
